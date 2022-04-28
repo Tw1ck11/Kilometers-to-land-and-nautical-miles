2486cd4bfd7680064ce3d4976e69e72283484a26
Tw1ck11 <qwentyn2222222222@gmail.com>
2022-04-28 22:36:12 +0200

    Kilometers to land and nautical miles

---
+++ Километры в сухопутные и морские мили/Километры в сухопутные и морские мили.cpp
@@ -0,0 +1,21 @@
+﻿// Километры в сухопутные и морские мили.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float kilometers, l_miles, n_miles;
+
+    cout << "This program converts kilometers to land and nautical miles" << endl;
+
+    cout << "Enter how many kilometers: ";
+    cin >> kilometers;
+
+    l_miles = kilometers / 1.609;
+    n_miles = kilometers / 1.852;
+
+    cout << "In " << kilometers << " kilometers there is " << l_miles << " land miles" << endl;
+    cout << "In " << kilometers << " kilometers there is " << n_miles << " nautical miles" << endl;
+}
